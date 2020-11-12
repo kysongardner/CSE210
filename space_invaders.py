ea9f22cddee0727e4a898e6132eab8e4d39f2fd8
kysongardner <kysongardner1@gmail.com>
2020-11-11 18:02:20 -0700

    added space invaders

---
+++ space_invaders.py
@@ -0,0 +1,33 @@
+# Actors Class:
+
+# battleship (A)
+# bullet (A)
+# aliens (A) - score_amount
+# barrier (A)
+# alien mother ship up top (G)
+# back ground (G)
+
+#Properties:
+# speed
+# color
+# shape/image
+
+# Functions:
+# move bullet
+# remove_object_from_screen
+
+
+
+# Game Play Info Class:
+# Score (B) functions - increment_score, reset_score
+# Lives (B) function - take_away_life, reset_lives
+
+# Game Play Class
+# main game loop (A)
+# start game (A)
+# end game (B)
+# reset (B)
+
+# Input Service Class - get input from the user (e.g. move left, move right, fire bullet)
+
+# Output Service Class - print all objects to the screen and remove aliens that have been shot
